d0d3bf01299f96aa306527cca6c5e00cfd8c5aa1
linschneider <136378548+linschneider@users.noreply.github.com>
2023-09-07 10:51:11 +0300

    Update app_script.sh

--- app_script.sh
+++ app_script.sh
@@ -14,10 +14,6 @@ pip install psycopg2
 python3 -m pip install psycopg2-binary
 sleep 1
 
-# Set environment variables if required (replace with your own)
-#export FLASK_APP=~/TerraformSkyProject/sky_flask_app/sky_flask_app.py
-#export FLASK_ENV=production
-
 # Run your Flask application (adjust the command as needed)
 flask --app=sky_flask_app.py run --host=0.0.0.0 --port=8080
 
